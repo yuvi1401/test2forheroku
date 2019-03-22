3cfca6a314790ec33ba10cd18b4b56b01558e4b5
yuvaraj kadam <yuvarajkadam@macusers-iMac-2.local>
2019-03-22 16:35:12 +0000

    dbconnection modified

--- dbconnection.py
+++ dbconnection.py
@@ -1,15 +1,36 @@
 import psycopg2
+import urllib.parse as urlparse
+import os
 from dbconfig_prod import user, password, database, host
 
+
 try:
-    connection = psycopg2.connect(
-    user=user,
-    password=password,
-    database=database,
-    host=host)
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
+
+        connection = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+
+    else:
+        connection = psycopg2.connect(
+        user=user,
+        password=password,
+        database=database,
+        host=host)
 
     cursor = connection.cursor()
 
 except:
     connection = None
     cursor = None
+
